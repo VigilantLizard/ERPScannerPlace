f6ed7df840cad11ebd4edbd38d6c6c892dd81263
VigilantLizard <nat20fen@gmail.com>
2024-10-30 21:55:34 -0500

    Added comment to explain what the script does.

--- AccountList.lua
+++ AccountList.lua
@@ -1,6 +1,7 @@
 -- Script by zonkerdoodle (@VigilantLizard).
 -- Put this in ReplicatedStorage as a ModuleScript.
 
+-- Uses my GitHub repository to pull the updated list of MFD-confirmed ERP accounts.
 local result = game:GetService("HttpService"):GetAsync("https://raw.githubusercontent.com/VigilantLizard/ERPLists/refs/heads/main/AccountList.bin")
 local list = result:split(",\n")
 local module = list
